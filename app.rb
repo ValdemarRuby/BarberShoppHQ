02e86c2a20fd5e2d1f888cdc2c9691aa1a157ab9
ValdemarRuby <wings6928@gmail.com>
2019-08-03 12:05:00 +0300

    Add client migration

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require "sinatra/activerecord"
+
+# подключение к бд
+set :database, "sqlite3:barbershopp.db"
+
+# создание сущности
+class Client < ActiveRecord::Base
+	
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"
---
+++ db/migrate/20190803085635_create_client.rb
@@ -0,0 +1,4 @@
+class CreateClient < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
