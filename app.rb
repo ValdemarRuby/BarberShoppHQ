d5f8d15aab3383c489dbc05f6f84ab337b937890
ValdemarRuby <wings6928@gmail.com>
2019-08-05 11:04:48 +0300

    Save client onformation

--- app.rb
+++ app.rb
@@ -13,7 +13,33 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
 	@barbers = Barber.all
 	erb :index
+end
+
+get '/visit' do
+  erb :visit
+end
+
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datestamp = params[:datestamp]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	user = Client.new :name => "#{@username}", :phone => "#{@phone}", :datestamp => "#{@datestamp}", :barber => "#{@barber}", :color => "#{@color}"
+	if user.new_record?
+		user.save
+	else
+		@error = 'You are already recorded!'
+	end
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datestamp}, #{@barber}, #{@color}"
 end
