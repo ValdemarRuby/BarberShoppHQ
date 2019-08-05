79708f6f0bed733405d5a117b0f2dc7706cb37ed
ValdemarRuby <wings6928@gmail.com>
2019-08-05 15:57:08 +0300

    Add length validation

--- app.rb
+++ app.rb
@@ -8,10 +8,10 @@ set :database, "sqlite3:barbershop.db"
 
 # создание сущности
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
-	validates :barber, presence: true
+	validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
@@ -27,17 +27,18 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new params[:client]
   erb :visit
 end
 
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	if c.save
+	@c = Client.new params[:client]
+	if @c.save
 		erb "<h2>Спасибо вы записались</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 
